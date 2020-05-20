459ca92dd69e2a9573f39ac8cc4d1cb30fe5b29d
bwarner88 <brian_warner25@yahoo.com>
2020-05-20 09:44:58 -0700

    stripped version

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import Vue from "vue";
 import UserPop from "./UserPop.vue";
-import UserPopImage from "./UserPopImage.vue";
 
 const previewComponent = {
   install: function(Vue, options) {
     Vue.component("user-pop", UserPop);
-    Vue.component("user-pop-image", UserPopImage);
   },
 };
 
